2703aeb06f34510011fd88fdb0ae0124deb9c05c
Akanksha Patel <a.patel@easternenterprise.com>
2021-12-04 16:43:30 +0530

    fixed the picture

--- time-tracking-dashboard/src/components/UserName/index.jsx
+++ time-tracking-dashboard/src/components/UserName/index.jsx
@@ -4,7 +4,7 @@ export const UserName = () => {
   return (
     <div className="p-2 inline sm:block">
       <div className="text-xs text-gray-300 inline">Report for  </div>
-      <div className="text-white text-2xl ml-12 sm:ml-0 inline sm:inline-block">Jeremy Robson</div>
+      <div className="text-white text-2xl ml-12 sm:ml-0">Jeremy Robson</div>
     </div>
   );
 };
