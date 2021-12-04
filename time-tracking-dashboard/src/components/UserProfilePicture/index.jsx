aec9993e58112f6eeb3c1c3c95164b71abe88b49
Akanksha Patel <a.patel@easternenterprise.com>
2021-12-04 22:48:42 +0530

    added css for the responsiveness

--- time-tracking-dashboard/src/components/UserProfilePicture/index.jsx
+++ time-tracking-dashboard/src/components/UserProfilePicture/index.jsx
@@ -4,7 +4,7 @@ import image from '../../images/image-geremy.png'
 export const UserProfilePicture = () => {
     return (
         <div className="pl-2 pt-4 inline sm:block">
-             <img alt="profile" src={image} className="bg-indigo-50 w-10 h-10 rounded-3xl inline sm:block" />
+             <img alt="profile" src={image} className="bg-indigo-50 w-12 h-12 rounded-3xl inline sm:block" />
         </div>
        
     )
