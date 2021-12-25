8861921c01833f839403ea9255a971b5f8a3f8b2
Akanksha Patel <a.patel@easternenterprise.com>
2021-12-25 20:10:45 +0530

    mobile and 1440px desktop css applied

--- time-tracking-dashboard/src/components/UserProfile/index.jsx
+++ time-tracking-dashboard/src/components/UserProfile/index.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 export const UserProfile = (props) => {
     return (
-        <div className="mx-10 sm:mx-40 sm:h-80 sm:w-40 w-72 h-44 border-0 font-body rounded-xl bg-indigo-900 sm:grid-cols-1">
+        <div className="mx-10 sm:mx-72 sm:h-80 sm:w-40 w-72 h-44 border-0 font-body rounded-xl bg-indigo-900 sm:grid-cols-1">
             {props.children}
         </div>
     )
