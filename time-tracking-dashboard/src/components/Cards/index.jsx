9ebfc111409d253d4a18abe143f73700236f6136
Akanksha Patel <a.patel@easternenterprise.com>
2021-12-25 22:02:47 +0530

    updated the css for large and xl screen

--- time-tracking-dashboard/src/components/Cards/index.jsx
+++ time-tracking-dashboard/src/components/Cards/index.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 export const Cards = (props) => {
   return (
-    <div className="lg:h-72 lg:w-4/5 lg:grid lg:grid-cols-3 lg:-ml-32 ml-8 pt-4 lg:pt-0 flex flex-col gap-8">
+    <div className="lg:h-72 lg:w-4/5 lg:grid lg:grid-cols-3 lg:gap-x-32 lg:gap-y-5 xl:gap-9 lg:-ml-32 ml-8 pt-4 lg:pt-0 flex flex-col gap-8">
       {props.children}
     </div>
   );
--- time-tracking-dashboard/src/components/UserProfile/index.jsx
+++ time-tracking-dashboard/src/components/UserProfile/index.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 export const UserProfile = (props) => {
     return (
-        <div className="mx-10 lg:mx-72 lg:h-80 lg:w-40 w-72 h-44 border-0 font-body rounded-xl bg-indigo-900 lg:grid-cols-1">
+        <div className="mx-10 lg:mx-44 lg:my-1 xl:my-0 xl:mx-72 lg:h-80 lg:w-40 w-72 h-44 border-0 font-body rounded-xl bg-indigo-900 lg:grid-cols-1 flex flex-col">
             {props.children}
         </div>
     )
