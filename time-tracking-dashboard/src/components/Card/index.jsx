23e3eed91885c40a39f3a3800ac985166ae227f1
Akanksha Patel <a.patel@easternenterprise.com>
2022-01-23 21:45:08 +0530

    refactored

--- time-tracking-dashboard/src/components/Card/index.jsx
+++ time-tracking-dashboard/src/components/Card/index.jsx
@@ -1,9 +1,9 @@
-import React from 'react'
+import React from "react";
 
 export const Card = (props) => {
-    return (
-        <div className="bg-pink-400 lg:w-40 lg:h-32 lg:mb-2 w-72 h-28 m-2 xl:m-0 rounded-xl flex-col gap-16">
-            {props.children}
-        </div>
-    )
-}
+  return (
+    <div className="bg-pink-400 lg:w-40 lg:h-32 lg:mb-2 w-72 h-28 m-2 xl:m-0 rounded-xl flex-col gap-16">
+      {props.children}
+    </div>
+  );
+};
